42f2c995a9dc11cbf235bd97f401c0e4a51d7d18
Rajkumar Kanakattu <Kanakatturavi7@gmail.com>
2023-07-02 22:35:08 +0100

    Update s3-notification-triggers.sh

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -16,7 +16,7 @@ role_name="s3-sns"
 email_address="Kanakatturajkumar6@gmail.com"
 
 # Create IAM Role for the project
-role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
+role_response=$(aws iam create-role --role-name s3-sns --assume-role-policy-document '{
   "Version": "2012-10-17",
   "Statement": [{
     "Action": "sts:AssumeRole",
@@ -51,7 +51,7 @@ echo "Bucket creation output: $bucket_output"
 aws s3 cp ./example_file.txt s3://"$bucket_name"/example_file.txt
 
 # Create a Zip file to upload Lambda Function
-zip -r s3-lambda-function.zip ./s3-lambda-function
+zip -r s3-function.zip ./s3-function
 
 sleep 5
 # Create a Lambda function
@@ -59,22 +59,22 @@ aws lambda create-function \
   --region "$aws_region" \
   --function-name $lambda_func_name \
   --runtime "python3.8" \
-  --handler "s3-lambda-function/s3-lambda-function.lambda_handler" \
+  --handler "s3-function/s3-function.lambda_handler" \
   --memory-size 128 \
   --timeout 30 \
   --role "arn:aws:iam::$aws_account_id:role/$role_name" \
-  --zip-file "fileb://./s3-lambda-function.zip"
+  --zip-file "fileb://./s3-function.zip"
 
 # Add Permissions to S3 Bucket to invoke Lambda
 aws lambda add-permission \
   --function-name "$lambda_func_name" \
-  --statement-id "s3-lambda-sns" \
+  --statement-id "s3-sns" \
   --action "lambda:InvokeFunction" \
   --principal s3.amazonaws.com \
   --source-arn "arn:aws:s3:::$bucket_name"
 
 # Create an S3 event trigger for the Lambda function
-LambdaFunctionArn="arn:aws:lambda:us-east-1:$aws_account_id:function:s3-lambda-function"
+LambdaFunctionArn="arn:aws:lambda:us-east-1:$aws_account_id:function:s3-function"
 aws s3api put-bucket-notification-configuration \
   --region "$aws_region" \
   --bucket "$bucket_name" \
